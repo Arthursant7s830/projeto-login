86927410c5575f31b187083bf1c2f12f8d82fdbc
Arthur Santos Lima <135383243+Arthursant7s830@users.noreply.github.com>
2025-07-22 17:27:40 -0300

    up

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
-alert('Olá!');// mensagem de boas vindas
-
 document.getElementById('entrar').addEventListener('click', function (e) {
-  e.preventDefault(); // Impede o envio do formulário
+  e.preventDefault(); 
 
   let email = document.getElementById('ilogin').value.trim();
   let senha = document.getElementById('isenha').value.trim();
@@ -11,10 +9,9 @@ document.getElementById('entrar').addEventListener('click', function (e) {
   } else {
     alert('Login simulado com sucesso!');
   }
-}) // conclui ou impede login, dependendo dos dados serem inseridos ou não
+}) 
 
 window.onload = function () {
   document.getElementById('ilogin').focus();
-}// abre a tela de login com foco já no email
-
+}
 
